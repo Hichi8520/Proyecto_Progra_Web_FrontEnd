8efcbef1e93ed6869f1e85550070d5e4a93f8b2a
Luis <hichi8520@gmail.com>
2021-02-18 16:49:02 -0600

    Barra de Navegación en página principal

--- BreakingSad/src/App.js
+++ BreakingSad/src/App.js
@@ -1,29 +1,19 @@
-import logo from './assets/images/logo.svg';
+//import logo from './assets/images/logo.svg';
+import React from 'react';
 import './assets/css/App.css';
-
-//importar componente
-import MiComponente from './components/MiComponente.js'
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
-var nombre = "Luis Roldan";
-var presentacion = <h2>hola soy {nombre}</h2>
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita <code>src/App.js</code> and save to reload.
-        </p>
-        {presentacion}
-
-        <section className="componentes">
-        <MiComponente/>
-      </section>
-
-      </header>
-      
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path='/' exact />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
