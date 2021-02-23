c41c7d25e7bb9724c817ce4976e7e25cde57ceb1
Luis <hichi8520@gmail.com>
2021-02-23 13:59:53 -0600

    CRUD básico de personajes

--- BreakingSad/src/components/pages/Characters.js
+++ BreakingSad/src/components/pages/Characters.js
@@ -7,11 +7,11 @@ import {Modal, ModalBody, ModalHeader, ModalFooter} from 'reactstrap';
 function Characters() {
 
     const dataCharacters =[
-        {id:1, name:"Walter White", nickname:"Heisenberg", occupation:"Chemistry Teacher", appearence:"1,2,3,4,5", portrayed:"Brian Cranston"},
-        {id:2, name:"Jesse Pinkman", nickname:"Cap n' Cook", occupation:"Meth Dealer", appearence:"1,2,3,4,5", portrayed:"Aaron Paul"},
-        {id:3, name:"Henry Schrader", nickname:"Hank", occupation:"DEA Agent", appearence:"1,2,3,4,5", portrayed:"Dean Norris"},
-        {id:4, name:"Skyler White", nickname:"Sky", occupation:"House wife", appearence:"1,2,3,4,5", portrayed:"Anna Gunn"},
-        {id:5, name:"Walter White Jr.", nickname:"Flynn", occupation:"Teenager", appearence:"1,2,3,4,5", portrayed:"RJ Mitte"},
+        {id:1, name:"Walter White", nickname:"Heisenberg", occupation:"Chemistry Teacher", portrayed:"Brian Cranston"},
+        {id:2, name:"Jesse Pinkman", nickname:"Cap n' Cook", occupation:"Meth Dealer", portrayed:"Aaron Paul"},
+        {id:3, name:"Henry Schrader", nickname:"Hank", occupation:"DEA Agent", portrayed:"Dean Norris"},
+        {id:4, name:"Skyler White", nickname:"Sky", occupation:"House wife", portrayed:"Anna Gunn"},
+        {id:5, name:"Walter White Jr.", nickname:"Flynn", occupation:"Teenager", portrayed:"RJ Mitte"},
     ];
     
     const [data, setData] = useState(dataCharacters);
@@ -24,33 +24,61 @@ function Characters() {
         name: '',
         nickname: '',
         occupation: '',
-        appearence: ''
+        portrayed: ''
       });
 
-      const handleChange=e=>{
+    const chooseCharacter=(elemento, caso)=>{
+        setSelectedCharacter(elemento);
+        (caso==='Edit')?setModalEdit(true):setModalDelete(true)
+    }
+
+    const handleChange=e=>{
         const {name, value}=e.target;
         setSelectedCharacter((prevState)=>({
           ...prevState,
           [name]: value
         }));
-      }
+    }
 
-      const edit=()=>{
+    const edit=()=>{
         var dataNueva=data;
         dataNueva.map(character=>{
           if(character.id===selectedCharacter.id){
+            character.name=selectedCharacter.name;
             character.nickname=selectedCharacter.nickname;
             character.occupation=selectedCharacter.occupation;
-            character.appearence=selectedCharacter.appearence;
+            character.portrayed=selectedCharacter.portrayed;
           }
         });
         setData(dataNueva);
         setModalEdit(false);
-      }
+    }
+
+    const deleted =()=>{
+        setData(data.filter(character=>character.id!==selectedCharacter.id));
+        setModalDelete(false);
+    }
+
+    const abrirModalInsertar=()=>{
+        setSelectedCharacter(null);
+        setModalInsert(true);
+    }
+
+    const insert =()=>{
+        var valueInsert=selectedCharacter;
+        valueInsert.id=data[data.length-1].id+1;
+        var newData = data;
+        newData.push(valueInsert);
+        setData(newData);
+        setModalInsert(false);
+    }
 
     return (
         <div className="Characters">
             <h2>Characters</h2>
+            <br/>
+            <button className="btn btn-success" onClick={()=>abrirModalInsertar()}>Insert</button>
+            <br/><br/>
             <table className="table table-bordered">
                 <thead>
                     <tr>
@@ -58,7 +86,7 @@ function Characters() {
                         <th>Name</th>
                         <th>Nickname</th>
                         <th>Occupation</th>
-                        <th>Appearence</th>
+                        <th>Portrayed</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -68,10 +96,10 @@ function Characters() {
                             <td>{elemento.name}</td>
                             <td>{elemento.nickname}</td>
                             <td>{elemento.occupation}</td>
-                            <td>{elemento.appearence}</td>
+                            <td>{elemento.portrayed}</td>
                             <td>
-                                <button className="btn btn-primary">Edit</button> {"   "}
-                                <button className="btn btn-danger">Delete</button>
+                                <button className="btn btn-primary" onClick={()=>chooseCharacter(elemento, 'Edit')}><i className='far fa-edit'/></button> {"   "}
+                                <button className="btn btn-danger" onClick={()=>chooseCharacter(elemento, 'Delete')}><i className='far fa-trash-alt'/></button>
                             </td>
                         </tr>
                     ))
@@ -87,15 +115,6 @@ function Characters() {
                 </ModalHeader>
                 <ModalBody>
                     <div className="form-group">
-                        <label>ID</label>
-                        <input
-                        className="form-control"
-                        readOnly
-                        type="text"
-                        name="id"
-                        value={selectedCharacter && selectedCharacter.id}
-                        />
-                        <br />
 
                         <label>Name</label>
                         <input
@@ -125,13 +144,14 @@ function Characters() {
                         value={selectedCharacter && selectedCharacter.occupation}
                         onChange={handleChange}
                         />
+                        <br />
 
-                        <label>Appearence</label>
+                        <label>Portrayed</label>
                         <input
                         className="form-control"
                         type="text"
-                        name="appearence"
-                        value={selectedCharacter && selectedCharacter.appearence}
+                        name="portrayed"
+                        value={selectedCharacter && selectedCharacter.portrayed}
                         onChange={handleChange}
                         />
                         <br />
@@ -139,7 +159,7 @@ function Characters() {
                 </ModalBody>
                 <ModalFooter>
                     <button className="btn btn-primary" onClick={()=>edit()}>
-                        Actualizar
+                        Save Changes
                     </button>
                     <button
                         className="btn btn-danger"
@@ -150,6 +170,89 @@ function Characters() {
                 </ModalFooter>
             </Modal>
 
+
+            <Modal isOpen={modalDelete}>
+                <ModalBody>
+                Are you sure you want to delete {selectedCharacter && selectedCharacter.name}?
+                </ModalBody>
+                <ModalFooter>
+                <button className="btn btn-danger" onClick={()=>deleted()}>
+                    Yes
+                </button>
+                <button
+                    className="btn btn-secondary"
+                    onClick={()=>setModalDelete(false)}
+                >
+                    No
+                </button>
+                </ModalFooter>
+            </Modal>
+
+
+            <Modal isOpen={modalInsert}>
+                <ModalHeader>
+                    <div>
+                        <h3>Insert new Character</h3>
+                    </div>
+                </ModalHeader>
+                <ModalBody>
+                    <div className="form-group">
+
+
+                        <label>Name</label>
+                            <input
+                            className="form-control"
+                            type="text"
+                            name="nombre"
+                            value={selectedCharacter ? selectedCharacter.name: ''}
+                            onChange={handleChange}
+                            />
+                        <br />
+
+                        <label>Nickname</label>
+                            <input
+                            className="form-control"
+                            type="text"
+                            name="nickname"
+                            value={selectedCharacter ? selectedCharacter.nickname: ''}
+                            onChange={handleChange}
+                            />
+                        <br />
+
+                        <label>Occupation</label>
+                            <input
+                            className="form-control"
+                            type="text"
+                            name="occupation"
+                            value={selectedCharacter ? selectedCharacter.occupation: ''}
+                            onChange={handleChange}
+                            />
+                        <br />
+
+                        <label>Portrayed by</label>
+                            <input
+                            className="form-control"
+                            type="text"
+                            name="portrayed"
+                            value={selectedCharacter ? selectedCharacter.portrayed: ''}
+                            onChange={handleChange}
+                            />
+                        <br />
+                    </div>
+                </ModalBody>
+                <ModalFooter>
+                    <button className="btn btn-primary"
+                    onClick={()=>insert()}>
+                        Insert
+                    </button>
+                <button
+                    className="btn btn-danger"
+                    onClick={()=>setModalInsert(false)}
+                >
+                    Cancel
+                </button>
+                </ModalFooter>
+            </Modal>
         </div>
     )
 }
